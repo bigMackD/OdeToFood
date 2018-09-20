9f1b130464fc1951c720a99c41eb920ef299ebe1
Maciek <maciek.d@me.com>
2018-09-20 18:05:05 +0200

    Added appsetting file and greeter interface

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _config;
+
+        public string GetMessageOfTheDay()
+        {
+            return _config["Greeting"];
+        }
+        public Greeter(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OdeToFood
@@ -15,10 +16,13 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app,
+                              IHostingEnvironment env,
+                              IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +31,8 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greet = greeter.GetMessageOfTheDay();
+                await context.Response.WriteAsync(greet);
             });
         }
     }
