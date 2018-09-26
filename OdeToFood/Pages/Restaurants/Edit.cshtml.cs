8386cc1d06d5f24658a4fd435179f83e14846ac4
Maciek <maciek.d@me.com>
2018-09-26 05:20:26 -0700

    Added connection edit page

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Models;
+using OdeToFood.Services;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private IRestaurantData _restaurantData;
+        public Restaurant Restaurant { get; set; }
+
+        public EditModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Restaurant = _restaurantData.Get(id);
+            _restaurantData.Get(id);
+            if(Restaurant == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return Page();
+        }
+    }
+}
